cdd50171ed58575f7ff28813c7fd5801752029dc
ssrinath <ssrinath@ariadnelabs.org>
2017-06-12 15:07:44 -0400

    setting path

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,12 +2,12 @@ sudo apt-get update
 sudo apt-get install git -y
 #sudo apt-get install golang-go -y
 sudo curl -O https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
-#sudo tar -xvf go1.8.3.src.tar.gz
+sudo tar -xvf go1.6.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.6.linux-amd64.tar.gz
 #sudo mv go /usr/local
 
-echo "export GOPATH=/home/vagrant/" >> ~/.profile
-echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> ~/.profile  
+echo "export GOPATH=/home/vagrant/" >> /home/vagrant/.bashrc
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> /home/vagrant/.bashrc 
 
 
 go get golang.org/x/tools/cmd/godoc
